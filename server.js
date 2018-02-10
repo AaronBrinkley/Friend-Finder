5ddc14443ad86f2d51d3a449b3e1414ba1840a54
Aaron <aaronl.brinkley@gmail.com>
2018-02-10 14:23:46 -0500

    heroku test #2

--- server.js
+++ server.js
@@ -1,23 +1,19 @@
 var express = require("express");
 
-console.log("hello")
+console.log("hello");
+
 var path = require('path');
-console.log("hello")
+
 
 var bodyParser = require('body-parser');
-console.log("hello")
 
 var htmlFile = require('./app/routing/htmlRoutes.js');
-console.log("hello")
 
 var apiFile = require('./app/routing/apiRoutes.js');
-console.log("hello")
 
 var friendsFile = require('./app/data/friends.js');
-console.log("hello")
 
 var friendsAPI = friendsFile.friends
-console.log("hello")
 
 
 var app = express();
