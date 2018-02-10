c654c2312bd91f07f92ec42f286dcbfaa354459f
Aaron <aaronl.brinkley@gmail.com>
2018-02-10 14:38:09 -0500

    heroku test 9

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -17,11 +17,11 @@ app.use(bodyParser.json());
 
 
 app.get('/', function(req, res) {
-    res.sendFile('/Users/Aaron Brinkley/Friend-Finder/app/public/home.html');
+    res.sendFile('..public/home.html');
 });
 
 app.get('/survey', function(req, res) {
-    res.sendFile('/Users/Aaron Brinkley/Friend-Finder/app/public/survey.html');
+    res.sendFile('..public/survey.html');
 });
 
     
