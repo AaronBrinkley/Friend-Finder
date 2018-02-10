e83907943f6328125efc1207acffeb4e34f10999
Aaron <aaronl.brinkley@gmail.com>
2018-02-10 14:38:59 -0500

    heroku test 10

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -17,11 +17,11 @@ app.use(bodyParser.json());
 
 
 app.get('/', function(req, res) {
-    res.sendFile('..public/home.html');
+    res.sendFile('../public/home.html');
 });
 
 app.get('/survey', function(req, res) {
-    res.sendFile('..public/survey.html');
+    res.sendFile('../public/survey.html');
 });
 
     
