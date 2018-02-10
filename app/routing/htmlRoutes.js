76989c7c10bf534710e9c352fe74575cfb802111
Aaron <aaronl.brinkley@gmail.com>
2018-02-10 14:50:29 -0500

    heroku test 13

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -25,7 +25,7 @@ app.get('/', function(req, res) {
 
 
 
-app.get('/', function(req, res) {
+app.get('/survey', function(req, res) {
     res.sendFile(path.join(__dirname,'../public', "survey.html"));
 });
 
